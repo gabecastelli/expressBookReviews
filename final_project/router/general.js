0d6f246a0fde814cd312ca68736ec8dc6571255a
Gabriele Castelli <gabrielecastelli@me.com>
2025-01-28 15:36:12 +0100

    Revert "Change books to array"
    This reverts commit 96b2e87346d514b5977b94fa8c9aef9704726972.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,31 +10,30 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", (req, res) => {
+public_users.get("/", function (req, res) {
     return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", (req, res) => {
-    return res.send(
-        JSON.stringify(books.filter(book => book.isbn === req.params.isbn))
-    );
+public_users.get("/isbn/:isbn", function (req, res) {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get book details based on author
-public_users.get("/author/:author", (req, res) => {
+public_users.get("/author/:author", function (req, res) {
     //Write your code here
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get all books based on title
-public_users.get("/title/:title", (req, res) => {
+public_users.get("/title/:title", function (req, res) {
     //Write your code here
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 //  Get book review
-public_users.get("/review/:isbn", (req, res) => {
+public_users.get("/review/:isbn", function (req, res) {
     //Write your code here
     return res.status(300).json({ message: "Yet to be implemented" });
 });

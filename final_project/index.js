8cc3c176af46f28728b9729af8f998e8ed22cbf0
Gabriele Castelli <gabrielecastelli@me.com>
2025-01-28 15:11:42 +0100

    Implement auth middleware

--- final_project/index.js
+++ final_project/index.js
@@ -19,9 +19,30 @@ app.use(
     })
 );
 
-app.use("/customer/auth/*", (req, res, next) => {});
+app.use("/customer/auth/*", (req, res, next) => {
+    if (!req.session.authorization) {
+        return res.status(403).send("User not logged in.");
+    }
+
+    const token = req.session.authorization["accessToken"];
+
+    jwt.verify(token, "access", (err, user) => {
+        if (err) {
+            return res.status(403).send("User not authenticated.");
+        }
+
+        req.user = user;
+        next();
+    })
+});
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
 app.listen(PORT, () => console.log("Server is running"));
+
+function userIsAuthenticated(username, password) {
+    return users.some(
+        (user) => user.username === username && user.passowrd === password
+    );
+}

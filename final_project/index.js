4f6a41e7118b2ffe478bead477139a910601effa
Gabriele Castelli <gabrielecastelli@me.com>
2025-01-28 15:00:13 +0100

    Fix imports, reformat

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+import { express } from "express";
+import { jwt } from "jsonwebtoken";
+import { session } from "express-session";
+import { users, authenticated as customer_routes } from "./router/auth_users";
+import { general as genl_routes } from "./router/general";
+
+const PORT = 5000;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+    "/customer",
+    session({
+        secret: "fingerprint_customer",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use("/customer/auth/*", (req, res, next) => {});
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
